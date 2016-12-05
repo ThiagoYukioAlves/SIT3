a3de0c447923a17b559d5edb2145167e5334eb7d
MarioGabriel <mgigamail@gmail.com>
2016-12-04 22:49:54 -0200

    Revert "Revert "Criacao alterar senha""
    This reverts commit fae1688df8f957e7ba69939959880bdb7fca6c5a.

--- SIT3-Java/src/java/usuario/Usuario.java
+++ SIT3-Java/src/java/usuario/Usuario.java
@@ -33,7 +33,7 @@ public class Usuario {
         try {
             DbTransaction tr = new DbTransaction();
             UsuarioData UserData = new UsuarioData();
-            UserData.alterarSenha(tr, usuario,senha);
+            UserData.alterarSenha(tr, usuario);
             return true;
         }
         catch (Exception e) {
